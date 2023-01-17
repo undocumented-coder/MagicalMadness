ec9b7f1842ed42d4014a554fcece52fc23a7b111
ster ster <mnohs23cmiller@mail.mnohs.org>
2023-01-17 06:32:29 -0600

    Added Zydis!

---
+++ MagicalMadness/src/disassembler/disassembler.cpp
@@ -0,0 +1,8 @@
+#include "disassembler.hpp"
+
+
+void my_function()
+{
+	disassembler_t abc{};
+
+}
---
+++ MagicalMadness/src/disassembler/disassembler.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+#include <cstdint>
+
+
+class disassembler_t
+{
+public:
+	disassembler_t() = default;
+	disassembler_t(const disassembler_t&) = delete;
+};
